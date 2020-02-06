8a4384a8853d2afb0c2ca41d8ebb548194606206
Jonah Jepsen <jonah.jepsen@gmail.com>
2020-02-05 21:03:43 -0600

    just realized the Y button was responsible for two commands. I changed the button for chasing the ball to X.

--- ramsetecommand/src/main/java/frc/robot/RobotContainer.java
+++ ramsetecommand/src/main/java/frc/robot/RobotContainer.java
@@ -90,7 +90,7 @@ public class RobotContainer {
     //new JoystickButton(m_driverController, Button.kY.value).whenPressed(() -> m_shooter.Shoot());
 
     new JoystickButton(m_driverController, Button.kY.value).whileHeld(new Shoot(m_shooter));
-    new JoystickButton(m_driverController, Button.kY.value).whileHeld(new Kuchota(m_robotDrive, m_limelight));
+    new JoystickButton(m_driverController, Button.kX.value).whileHeld(new Kuchota(m_robotDrive, m_limelight));
   }
 
 
